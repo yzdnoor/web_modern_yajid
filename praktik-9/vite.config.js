6a36e30eb5f7611c0db3ff723c3a5404a80d029b
yzdnoor <muhammadyazidnoorn@gmail.com>
2025-12-15 19:42:16 +0700

    update build & vite config praktik 2-13

--- praktik-9/vite.config.js
+++ praktik-9/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/web_modern_yajid/praktik-9/',
+  base: './',
   plugins: [react()],
 })
